2ba1da20c7800d8e49e46d9e761e9747c8a415c6
Christopher Schlenker <schlenkerc18@hanover.edu>
2017-11-06 20:17:01 -0600

    planned code for players class close issue #2

--- app/src/main/java/com/hfad/euchreai/VirtualPlayers.java
+++ app/src/main/java/com/hfad/euchreai/VirtualPlayers.java
@@ -5,4 +5,18 @@ package com.hfad.euchreai;
  */
 
 public class VirtualPlayers {
+    int player1, player2, player3;
+
+    public void callTrumpOrPass() {
+        // write code to pass, play, or go alone
+    }
+
+    public void callTrumpOrPass2() {
+        // write code to choose a suit after trump has been pushed over
+    }
+
+    public void playCard() {
+        // write code to choose card to play
+        // want to develop simple heuristics before implementing AI
+    }
 }
