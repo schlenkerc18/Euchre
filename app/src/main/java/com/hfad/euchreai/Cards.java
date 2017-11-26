3103c75abda9ebd209393608c6e4935b0aacd7e4
Christopher Schlenker <schlenkerc18@hanover.edu>
2017-11-25 19:33:20 -0600

    added functionality to Cards ref issue #4

--- app/src/main/java/com/hfad/euchreai/Cards.java
+++ app/src/main/java/com/hfad/euchreai/Cards.java
@@ -41,14 +41,73 @@ public class Cards {
     }
 
     public int biddingValue(SUIT trump) {
-        SUIT sameColor = getSameColorSuit(trump);
+        SUIT sameColor= getSameColorSuit(trump);
 
-        return 5;
+        if(this.suit == trump){
+            switch(this.value){
+                case 14: //trump ace
+                    return 10;
+                case 13: //trump king
+                    return 8;
+                case 12: //trump queen
+                    return 7;
+                case 11: //trump jack
+                    return 12;
+                case 10: //trump ten
+                    return 5;
+                case 9: //trump nine
+                    return 4;
+                default:
+                    return -1;
+            }
+        } else if (this.suit == sameColor && this.value == 11){
+                return 11;
+          }
+            switch(this.value){
+                case 14: //random ace
+                    return 6;
+                case 13: //random king
+                    return 2;
+            }
+            return 0;
     }
 
     public int cardValue(SUIT trump, SUIT lead) {
-        SUIT sameColor = getSameColorSuit(trump);
+        SUIT sameColor= getSameColorSuit(trump);
 
-        return 5;
+        if (this.suit == trump){
+            if(this.value == 11) //Jack of trump suit
+                return 44;
+            return this.value + 28;
+        }
+        if ((this.suit == sameColor) && (this.value == 11)) //Jack of same color
+        {
+            return 43;
+        }
+        if (this.suit == lead)
+        {
+            return this.value + 14;
+        }
+        return this.value;
+    }
+
+    public int cardValueNotLead(SUIT trump) {
+        SUIT sameColor= getSameColorSuit(trump);
+
+        if (this.suit == trump){
+            if(this.value == 11) //Jack of trump suit
+                return 30;
+            return this.value + 14;
+        }
+        if ((this.suit == sameColor) && (this.value == 11)) //Jack of same color
+        {
+            return 29;
+        }
+        return this.value;
+    }
+
+    public boolean greater(Cards other, SUIT trump, SUIT lead) {
+        
+        return this.cardValue(trump, lead) > other.cardValue(trump, lead);
     }
 }
