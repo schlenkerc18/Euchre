242004a3fc6e48ea2f866c0895eca9769edd5912
Christopher Schlenker <schlenkerc18@hanover.edu>
2017-12-05 00:09:59 -0600

    added playCards function ref issue #6
    Still need way to access what the leading suit is and what the trump is for that trick

--- app/src/main/java/com/hfad/euchreai/VirtualPlayer.java
+++ app/src/main/java/com/hfad/euchreai/VirtualPlayer.java
@@ -10,13 +10,27 @@ import java.util.ArrayList;
 
 public class VirtualPlayer {
     public String name;
+    public ArrayList<Cards> hand;
 
     public VirtualPlayer(String name) {
         this.name = name;
+        hand = new ArrayList<Cards>();
     }
 
     public Cards playCard(Cards card){
-        return card; //stub function
+        boolean[] playableTrueFalse = this.getPlayableCards(leadingSuit, trump);
+        ArrayList<Integer> playableCards = new ArrayList<Integer>();
+        for (int i = 0 ; i < playableTrueFalse.length;i++)
+        {
+            boolean b = playableTrueFalse[i];
+            if (b == true)
+                playableCards.add(i);
+        }
+        Random random = new Random();
+        int r = random.nextInt(playableCards.size());
+        Cards c = hand.get(playableCards.get(r));
+        this.removeCardFromHand(c);
+        return c;
     }
 
     public boolean pickUp(Cards kitty){
