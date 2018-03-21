57a885eabedc29e988de404323e3352e02c095b6
Christopher <schlenkerc18@hanover.edu>
2018-03-20 22:07:07 -0500

    No longer using passPickUpDialog

--- app/src/main/java/com/hfad/euchreai/PassPickUpDialog.java
+++ app/src/main/java/com/hfad/euchreai/PassPickUpDialog.java
@@ -7,12 +7,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import static com.hfad.euchreai.GameActivity.iv_card10;
 
 /**
  * Created by Schlenker18 on 3/8/2018.
  */
 
-public class PassPickUpDialog extends Dialog implements android.view.View.OnClickListener {
+public class PassPickUpDialog extends Dialog {
     public Activity c;
     public Dialog d;
     public Button pass, pickUp;
@@ -34,23 +37,51 @@ public class PassPickUpDialog extends Dialog implements android.view.View.OnClic
         pass = (Button) findViewById(R.id.pass_button2);
         pickUp = (Button) findViewById(R.id.pickup_button);
 
-        pass.setOnClickListener(this);
-        pickUp.setOnClickListener(this);
+        //pass.setOnClickListener(this);
+        //pickUp.setOnClickListener(this);
+
+        pass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GameSetUp.humanPreRoundPass();
+                Log.v("----PsPUpDialog45----", "User passed");
+                dismiss();
+            }
+        });
+
+        pickUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GameSetUp.humanPreRoundCall();
+                //need to tell player to choose card to discard
+                GameActivity.setUpDiscard();
+                //GameActivity.discardListener();
+                //GameSetUp.assignImage(GameSetUp.currentRound.turnedUpCard.toString(), iv_card10);
+                Log.v("--PsPUpDialog49--", "dealerNeedstoDiscard:" + Round.dealerNeedsToDiscard);
+                Log.v("----PsPUpDialog50----", "User picked up");
+                dismiss();
+            }
+        });
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.pass_button2:
-                Log.v("----PsPUpDialog40----", "User passed");
-                dismiss();
-            case R.id.pickup_button:
-                Log.v("----PsPUpDialog43----", "User picked up");
-                dismiss();
-                break;
-            default:
-                break;
-        }
-        dismiss();
-    }
+//    @Override
+//    public void onClick(View v) {
+//        switch (v.getId()) {
+//            case R.id.pass_button2:
+//                GameSetUp.humanPreRoundPass();
+//                Log.v("----PsPUpDialog45----", "User passed");
+//                dismiss();
+//            case R.id.pickup_button:
+//                GameSetUp.humanPreRoundCall();
+//                //need to tell player to choose card to discard
+//                GameActivity.setUpDiscard();
+//                Log.v("--PsPUpDialog49--", "dealerNeedstoDiscard:" + Round.dealerNeedsToDiscard);
+//                Log.v("----PsPUpDialog50----", "User picked up");
+//                dismiss();
+//                break;
+//            default:
+//                break;
+//        }
+//        dismiss();
+//    }
 }
