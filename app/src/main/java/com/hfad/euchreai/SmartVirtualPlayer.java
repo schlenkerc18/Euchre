182514bb0d78d9da4d662c264bbc2415c3793e48
Christopher <schlenkerc18@hanover.edu>
2018-03-26 09:15:29 -0500

    AI is now making smarter decisions

--- app/src/main/java/com/hfad/euchreai/SmartVirtualPlayer.java
+++ app/src/main/java/com/hfad/euchreai/SmartVirtualPlayer.java
@@ -24,7 +24,7 @@ public class SmartVirtualPlayer extends Player {
                 choices.add(this.hand.get(i));
             }
 
-        Log.v("--SVP27--", "choices: " + choices.toString());
+        //Log.v("--SVP27--", "choices: " + choices.toString());
 
         if (trick.leadingSuit == null){
             for (int i = 0; i < choices.size(); i++){
@@ -36,7 +36,7 @@ public class SmartVirtualPlayer extends Player {
             return cardToPlayMax;
         }
 
-        Log.v("--SVP39--", "Choices.size(): " + choices.size());
+        //Log.v("--SVP39--", "Choices.size(): " + choices.size());
 
         for (int i = 0; i < choices.size(); i++){
             if (cardToPlayMin == null) {
@@ -61,40 +61,147 @@ public class SmartVirtualPlayer extends Player {
     }
 
     public boolean pickUp(Cards kitty){
-        Random random = new Random();
-        return random.nextBoolean();
+        ArrayList<Cards> hand = super.hand;
+        Cards.SUIT choice = null;
+        int max = 0;
+
+        if (handValue(hand,Cards.SUIT.CLUBS)>max) {
+            choice = Cards.SUIT.CLUBS;
+            max = handValue(hand, Cards.SUIT.CLUBS);
+        }
+
+        if (handValue(hand, Cards.SUIT.SPADES)>max) {
+            choice = Cards.SUIT.SPADES;
+            max = handValue(hand, Cards.SUIT.SPADES);
+        }
+
+        if (handValue(hand, Cards.SUIT.HEARTS)>max) {
+            choice = Cards.SUIT.HEARTS;
+            max = handValue(hand, Cards.SUIT.HEARTS);
+        }
+
+        if (handValue(hand, Cards.SUIT.DIAMONDS)>max) {
+            choice = Cards.SUIT.DIAMONDS;
+            max = handValue(hand, Cards.SUIT.DIAMONDS);
+        }
+
+        if (max > 22 && (choice == kitty.suit)) {
+            return true;
+        }
+
+        return false;
     }
 
     public boolean pickUpAsDealer(Cards kitty){
-        Random random = new Random();
-        return random.nextBoolean();
+        ArrayList<Cards> hand = super.hand;
+        int max = Math.max(Math.max(handValue(hand, Cards.SUIT.CLUBS), handValue(hand, Cards.SUIT.SPADES)),
+                Math.max(handValue(hand, Cards.SUIT.DIAMONDS), handValue(hand, Cards.SUIT.HEARTS)));
+        hand.add(kitty);
+        for (int i = 0; i < 5;i++){
+            Cards discard = hand.remove(0);
+            int val = (handValue(hand, kitty.suit));
+
+            if (val > max){
+                hand.remove(kitty);
+                hand.add(discard);
+                return true;
+            }
+            hand.add(discard);
+        }
+        hand.remove(kitty);
+
+        if (handValue(hand, kitty.suit) > 35) {
+            return true;
+        }
+
+        return false;
     }
 
     public Cards discardDecider(Cards kitty) {
-        Random random = new Random();
-        int ind = random.nextInt(6);
-        if (ind == 5)
-            return kitty;
-        else
-            return this.hand.get(ind);
-    }
+        ArrayList<Cards> hand = super.hand;
+        Cards ret = kitty;
+        int max = Math.max(Math.max(handValue(hand, Cards.SUIT.CLUBS),handValue(hand, Cards.SUIT.SPADES)),
+                Math.max(handValue(hand, Cards.SUIT.DIAMONDS),handValue(hand, Cards.SUIT.HEARTS)));
+        hand.add(kitty);
 
-    public Cards.SUIT trumpDecider(Cards.SUIT invalid) {
-        Cards.SUIT ret = invalid;
-        while (ret == invalid) {
-            Random random = new Random();
-            ret = Cards.SUIT.values()[random.nextInt(4)];
+        for (int i = 0; i < 5; i++){
+            Cards discard = hand.remove(0);
+            int val = (handValue(hand, kitty.suit));
+
+            if (val > max) {
+                max = val;
+                ret = discard;
+            }
+            hand.add(discard);
         }
+
+        hand.remove(kitty);
         return ret;
     }
 
+    public Cards.SUIT trumpDecider(Cards.SUIT invalid) {
+        ArrayList<Cards> hand = super.hand;
+        Cards.SUIT choice = null;
+        int max = 0;
+
+        if (invalid != Cards.SUIT.CLUBS && (handValue(hand, Cards.SUIT.CLUBS)>max)){
+            choice = Cards.SUIT.CLUBS;
+            max = handValue(hand, Cards.SUIT.CLUBS);
+        }
+
+        if (invalid != Cards.SUIT.SPADES && (handValue(hand, Cards.SUIT.SPADES)>max)){
+            choice = Cards.SUIT.SPADES;
+            max = handValue(hand, Cards.SUIT.SPADES);
+        }
+
+        if (invalid != Cards.SUIT.HEARTS && (handValue(hand, Cards.SUIT.HEARTS)>max)){
+            choice = Cards.SUIT.HEARTS;
+            max = handValue(hand,Cards.SUIT.HEARTS);
+        }
+
+        if (invalid != Cards.SUIT.DIAMONDS && (handValue(hand, Cards.SUIT.DIAMONDS)>max)){
+            choice = Cards.SUIT.DIAMONDS;
+            max = handValue(hand, Cards.SUIT.DIAMONDS);
+        }
+
+        return choice;
+    }
+
     public boolean callDecider(Cards.SUIT invalid){
-        Random random = new Random();
-        return random.nextBoolean();
+        ArrayList<Cards> hand = super.hand;
+        int max = 0;
+
+        if (invalid != Cards.SUIT.CLUBS && (handValue(hand, Cards.SUIT.CLUBS)>max)){
+            max = handValue(hand, Cards.SUIT.CLUBS);
+        }
+
+        if (invalid != Cards.SUIT.SPADES && (handValue(hand, Cards.SUIT.SPADES)>max)){
+            max = handValue(hand, Cards.SUIT.SPADES);
+        }
+
+        if (invalid != Cards.SUIT.HEARTS && (handValue(hand, Cards.SUIT.HEARTS)>max)){
+            max = handValue(hand, Cards.SUIT.HEARTS);
+        }
+
+        if (invalid != Cards.SUIT.DIAMONDS && (handValue(hand, Cards.SUIT.DIAMONDS)>max)){
+            max = handValue(hand, Cards.SUIT.DIAMONDS);
+        }
+
+        return max > 22;
     }
 
     public boolean goAloneDecider(Cards.SUIT trump){
-        Random random = new Random();
-        return random.nextBoolean();
+        //stub value being passed being going alone is buggy
+        return handValue(super.hand,trump) > 150;
+    }
+
+    public int handValue (ArrayList<Cards> hand, Cards.SUIT trump) {
+        int val = 0;
+
+        for(int i = 0; i < 5;i++) {
+            val += hand.get(i).biddingValue(trump);
+        }
+
+        return val;
     }
 }
